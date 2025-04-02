4039ca754637db7852110255c1a650605b3724b6
henryque <henryquenonatosilva@gmail.com>
2025-04-02 09:52:43 -0300

    mvc

---
+++ rotas/Controller.php
@@ -0,0 +1,12 @@
+<?php 
+
+class Controller{
+
+    //Função para carregar a view
+    public function carregarViews($views, $dados = array()){
+        extract($dados);
+
+        require_once '..app/views/'.$views.'php';
+
+    }
+}
