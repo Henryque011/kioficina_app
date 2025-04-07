5091f44b917c2d5e56dd0fa22c0e422071202eee
henryque <henryquenonatosilva@gmail.com>
2025-04-07 09:07:37 -0300

    login controller

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -17,9 +17,24 @@ class LoginController extends Controller
         $email = $_POST['email'] ?? null;
         $senha = $_POST['senha'] ?? null;
 
-        //fazer a requisiçã da API DE LOGIN
+        //fazer a requisição da API DE LOGIN
         $url = BASE_API . "login?email_cliente=$email&senha_cliente=$senha";
+        // recebe os dados em campos 
         $response = file_get_contents($url);
+        // separa os dados em 'campos'
         $data = json_decode($response, true);
+
+        if (isset($dara['token'])) {
+            $idToken = json_decode(base64_decode($data['token']), true);
+            $id_cliente = $idToken['id'] ?? null;
+
+            $_SESSION['token'] = $data['token'];
+            $_SESSION['id_cliente'] = $id_cliente;
+            header("location: " . BASE_URL . "index.php?url=menu");
+            exit;
+        }else {
+            header("location: " . BASE_URL . "index.php?url=login");
+            exit;
+        }
     }
 }
