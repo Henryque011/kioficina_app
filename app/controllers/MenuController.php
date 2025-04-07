e242ad5f5634afd66a955d9aff27e8129428a4ed
henryque <henryquenonatosilva@gmail.com>
2025-04-07 09:48:39 -0300

    menu controller

--- app/controllers/MenuController.php
+++ app/controllers/MenuController.php
@@ -5,9 +5,31 @@ class MenuController extends Controller
     public function index()
     {
 
+        if (!isset($_SESSION['id_cliente'])) {
+            header("Location: " . BASE_URL . "index.php?url=login");
+            exit;
+        }
+
+        $clienteId = $_SESSION['id_cliente'];
+
+        if (!$clienteId) {
+            session_destroy();
+            header("Location: " . BASE_URL . "index.php?url=login");
+            exit;
+        }
+
+        // buscar cliente na api
+        $url = BASE_API  . "cliente/$clienteId";
+        // recebe os dados em campos 
+        $response = file_get_contents($url);
+        // separa os dados em 'campos'
+        $cliente = json_decode($response, true);
+
         $dados = array();
         $dados['titulo'] = 'kiOficina - Menu';
-        
+
+        $dados['nome_cliente'] = $cliente['nome_cliente'] ?? 'CLiente';
+
         $this->carregarViews('menu', $dados);
     }
 }
