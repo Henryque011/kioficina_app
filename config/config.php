24cd7efabefad81d6d35f9077d947b898241c2ab
henryque <henryquenonatosilva@gmail.com>
2025-04-07 11:49:26 -0300

    config

--- config/config.php
+++ config/config.php
@@ -11,10 +11,10 @@ define('BASE_API', 'https://360criativo.com.br/api/');
 
 //Sisitema para carregametno automatico das classes
 spl_autoload_register(function ($class) {
-    if (file_exists('..app/controllers/' . $class . '.php')) {
+    if (file_exists('../app/controllers/' . $class . '.php')) {
         require_once '../app/controllers/' . $class . '.php';
     }
-    if (file_exists('..rotas/' . $class . '.php')) {
-        require_once '..rotas/' . $class . '.php';
+    if (file_exists('../rotas/' . $class . '.php')) {
+        require_once '../rotas/' . $class . '.php';
     }
 });
