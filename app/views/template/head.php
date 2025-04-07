f4e134b4d2ebc5c547f77c6e2a0ba491a5e4c1c7
henryque <henryquenonatosilva@gmail.com>
2025-04-07 11:49:21 -0300

    head

--- app/views/template/head.php
+++ app/views/template/head.php
@@ -8,6 +8,6 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="stylesheet" href="public/assets/css/reset.css">
-    <link rel="stylesheet" href="public/assets/css/style.css">
+    <link rel="stylesheet" href="http://localhost/kioficina_app/public/assets/css/reset.css">
+    <link rel="stylesheet" href="http://localhost/kioficina_app/public/assets/css/style.css">
 </head>
