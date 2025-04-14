cf31b36dde3ef37fb9a890ff37c7ba99068357f4
henryque <henryquenonatosilva@gmail.com>
2025-04-14 09:43:02 -0300

    LoginController

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -19,22 +19,43 @@ class LoginController extends Controller
 
         //fazer a requisição da API DE LOGIN
         $url = BASE_API . "login?email_cliente=$email&senha_cliente=$senha";
+
+        // Reconhecimento da chave(Inicializa uma sessão cURL)
+        $ch = curl_init($url);
+
+        // Definir que o conteudo venha com a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
         // recebe os dados em campos 
-        $response = file_get_contents($url);
+        // $response = file_get_contents($url); troca na forma de resposta
+        $response = curl_exec($ch);
+
         // separa os dados em 'campos'
-        $data = json_decode($response, true);
+        // Obtém o código HTTP da resposta (200, 400, 401)
 
-        if (isset($dara['token'])) {
-            $idToken = json_decode(base64_decode($data['token']), true);
-            $id_cliente = $idToken['id'] ?? null;
+        // $data = json_decode($response, true);
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
-            $_SESSION['token'] = $data['token'];
-            $_SESSION['id_cliente'] = $id_cliente;
-            header("location: " . BASE_URL . "index.php?url=menu");
-            exit;
+        // Encerra a sessão Curl
+        curl_close($ch);
+
+        if ($statusCode == 200) {
+
+            $data = json_decode($response, true);
+            if (!empty($data['token'])) {
+                // $idToken = json_decode(base64_decode($data['token']), true);
+                // $id_cliente = $idToken['id'] ?? null;
+
+                $_SESSION['token'] = $data['token'];
+                // $_SESSION['id_cliente'] = $id_cliente;
+                header("location: " . BASE_URL . "index.php?url=menu");
+                exit;
+            } else {
+                header("location: " . BASE_URL . "index.php?url=login");
+                exit;
+            }
         } else {
-            header("location: " . BASE_URL . "index.php?url=login");
-            exit;
+            echo "login Inválido";
         }
     }
 }
