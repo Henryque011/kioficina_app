dea2aec11e3b4e552aa9b67a2bb39c867e780aa7
henryque <henryquenonatosilva@gmail.com>
2025-04-14 09:59:24 -0300

    menu controller com nova estrutura na api

--- app/controllers/MenuController.php
+++ app/controllers/MenuController.php
@@ -5,23 +5,46 @@ class MenuController extends Controller
     public function index()
     {
 
-        if (!isset($_SESSION['id_cliente'])) {
+        if (!isset($_SESSION['token'])) {
             header("Location: " . BASE_URL . "index.php?url=login");
             exit;
         }
 
-        $clienteId = $_SESSION['id_cliente'];
+        $$dadosToken = $_SESSION['token'];
 
-        if (!$clienteId) {
+        if (!$dadosToken) {
             session_destroy();
+            unset($_SESSION['token']);
             header("Location: " . BASE_URL . "index.php?url=login");
             exit;
         }
 
         // buscar cliente na api
-        $url = BASE_API  . "cliente/$clienteId";
-        // recebe os dados em campos 
-        $response = file_get_contents($url);
+        $url = BASE_API  . "cliente/" . $dadosToken['id'];
+
+       // Reconhecimento da chave(Inicializa uma sessão cURL)
+        $ch = curl_init($url);
+
+        // Definir que o conteudo venha com a string
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [ 
+            'authorization: Bearer '.$_SESSION['token']
+        ]);
+        // recebe os dados dessa solicitação
+        $response = curl_exec($ch);
+
+        // Obtém o código HTTP da resposta (200, 400, 401)
+        $statusCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        // Encerra a sessão Curl
+        curl_close($ch);
+
+        if($statusCode != 200){
+            echo "Erro ao buscar cliente na API.\n
+            Código HTTP: $statusCode";
+        }
+
         // separa os dados em 'campos'
         $cliente = json_decode($response, true);
 
