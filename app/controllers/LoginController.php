eafffde81f9b9ac8f34c0cde29b46d187ff37d0c
henryque <henryquenonatosilva@gmail.com>
2025-04-14 10:58:24 -0300

    sixtase login

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -55,7 +55,7 @@ class LoginController extends Controller
                 exit;
             }
         } else {
-            echo "login Inválido";
+            echo "login Inválido - ";
         }
     }
 }
