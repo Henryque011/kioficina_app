d0a868c07e2c49bd317b9c606ef6a48efa7d28f3
henryque <henryquenonatosilva@gmail.com>
2025-05-06 11:05:44 -0300

    php mensagem de erro ou acerto

--- app/views/esqueci_senha.php
+++ app/views/esqueci_senha.php
@@ -7,9 +7,18 @@ require_once(__DIR__ . '/template/head.php');
 ?>
 
 <body>
+
     <section class="esqueci_senha">
         <article class="site">
             <div class="container">
+
+                <?php if (!empty($_SESSION['flash']) && is_array($_SESSION['flash'])): ?>
+                    <div class="alert <?= $_SESSION['flash']['tipo'] ?>">
+                        <?= $_SESSION['flash']['mensagem'] ?>
+                    </div>
+                    <?php unset($_SESSION['flash']); ?>
+                <?php endif; ?>
+
                 <h2>Recupear senha</h2>
                 <form action="index.php?url=login/enviarRecuperacao" method="POST">
                     <label for="email">E-mail cadastrado:</label>
@@ -17,6 +26,8 @@ require_once(__DIR__ . '/template/head.php');
 
                     <input type="submit" value="Enviar link" class="btn-link">
                 </form>
+
+
             </div>
             <p><a href="<?php echo BASE_URL; ?>index.php?url=login/">voltar ao login</a></p>
 
