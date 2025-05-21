27e72f10957135d9445f247baf883a239a45fc26
henryque <henryquenonatosilva@gmail.com>
2025-05-21 11:55:06 -0300

    view agendamento

--- app/views/agendamento.php
+++ app/views/agendamento.php
@@ -12,43 +12,50 @@ require_once('template/head.php')
         <article class="site">
             <div class="space"></div>
             <h2>faça seu agendamento</h2>
-            <div class="form-conteiner <?php echo $statusClass; ?>">
-                <form action="<?php echo BASE_URL; ?>index.php?url=" method="POST">
-                    <input type="hidden" name="id_Agendamento" value="<?php echo htmlspecialchars($Agendamento['id']); ?>"
-                        <label class="form-label">veiculo:</label>
-                    <select class="form-select mb-3">
-                        <option selected value="<?php echo htmlspecialchars($agendamento['veiculo']); ?>" <?php echo htmlspecialchars($agendamento['veiculo'] ?? 'Honda Motor Co. Ltd. - Honda Civic'); ?></option>
-                        <option>2</option>
-                        <option>3</option>
-                    </select>
 
-                    <label class="form-label">Data:</label>
-                    <input type="date" name="data" value="<?php echo htmlspecialchars($agendamento['data'] ?? ''); ?>">
+            <div class="container">
 
-                    <label class="form-label">Hora:</label>
-                    <select class="form-select mb-3">
-                        <option><?php
-                                $horarios = ['08:00', '09:00', '10:00'];
-                                foreach ($horarios as $hora) {
-                                    $selected = ($agendamento['hora'] ?? '') === $hora ? 'selected' : '';
-                                    echo "<option value=\"$hora\" $selected>$hora</option>";
-                                }
-                                ?>/option>
-                    </select>
+                <form method="POST">
+                    <div>
+                        <label for="id_veiculo">Veículo:</label>
+                        <select name="id_veiculo" id="id_veiculo" required>
+                            <option value="">Selecione o veículo</option>
+                            <?php foreach ($veiculos as $veiculo): ?>
+                                <option value="<?= $veiculo['id_veiculo'] ?>"><?= $veiculo['nome_modelo'] ?> - <?= $veiculo['cor_veiculo'] ?> - <? $veiculo['ano_veiculo'] ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
 
-                    <label class="form-label">Funcionário:</label>
-                    <select class="form-select mb-3">
-                        <option value=" selected">Selecione um funcionário</option>
-                        <option> <?php foreach ($funcionarios as $funcionario): ?>
-                        <option value="<?= htmlspecialchars($funcionario['id']); ?>"
-                            <?= ($funcionario['id'] == $agendamento['funcionario_id']) ? 'selected' : ''; ?>>
-                            <?= htmlspecialchars($funcionario['nome']); ?>
-                        </option>
-                    <?php endforeach; ?></option>
-                    <option>Maria</option>
-                    </select>
+                    <div>
+                        <label for="data_agenda">Data:</label>
+                        <input type="date" name="data_agenda" id="data_agenda" value="<?= date('Y-m-d') ?>" required>
+                    </div>
 
-                    <button type="submit" class="btn btn-agendar">Agendar </button>
+                    <div>
+                        <label for="hora_agenda">Hora:</label>
+                        <select name="hora_agenda" id="hora_agenda" required>
+                            <option value="">Selecione a hora</option>
+                            <option value="08:00">08:00</option>
+                            <option value="09:00">09:00</option>
+                            <option value="10:00">10:00</option>
+                            <option value="11:00">11:00</option>
+                            <option value="12:00">12:00</option>
+                        </select>
+                    </div>
+
+                    <div>
+                        <label>Funcionário:</label>
+                        <select name="id_funcionario" id="is_funcionario" required>
+                            <option value="">Selecione o funcionário</option>
+                            <?php foreach ($funcionarios as $funcionarios): ?>
+                                <option value="<?php $funcionario['id_funcioanrio'] ?>"><?php $funcionario['nome_funcioanrio'] ?></option>
+                            <?php endforeach ?>
+                        </select>
+                    </div>
+
+                    <div>
+                        <input type="submit" value="Agendar">
+                    </div>
                 </form>
             </div>
             <div class="conteiner" style="display: flex; justify-content: center; text-align: center; text-align: center;">
